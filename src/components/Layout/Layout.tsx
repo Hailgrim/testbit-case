3d3b68280303023e06e34a0c32c92c091835b201
Hailgrim <rasstrelyat@gmail.com>
2023-12-25 21:54:43 +0400

    Drawer fix

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,8 @@
 import styled from 'styled-components';
 
 const Layout = styled.div`
-  min-height: 100vh;
+  height: 100vh;
+  overflow: auto;
   background-color: var(--color-black);
   display: flex;
   flex-direction: column;
